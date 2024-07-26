3a0639a8b8a49bb6d2e574d975baea41c5d0c9ba
Majed1945 <majedbiazid@gmail.com>
2024-07-26 14:49:22 +0300

    Repo setup

---
+++ src/screens/EditCustomer/Index.js
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const EditCustomer = () => {
+  return (
+    <View>
+      <Text>EditCustomer</Text>
+    </View>
+  );
+};
+
+export default EditCustomer;
---
+++ src/screens/ShowCustomer/Index.js
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const ShowCustomer = () => {
+  return (
+    <View>
+      <Text>ShowCustomer</Text>
+    </View>
+  );
+};
+
+export default ShowCustomer;
