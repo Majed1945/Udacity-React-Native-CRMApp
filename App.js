a79e9018817a6165178619c85cce8157457edef9
Majed1945 <majedbiazid@gmail.com>
2024-07-26 15:07:27 +0300

    Stack navigation setup

--- App.js
+++ App.js
@@ -1,20 +1,8 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import "./gesture-handler";
+import Index from "./src/navigation/Index";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+const App = () => {
+  return <Index />;
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
---
+++ src/navigation/Index.js
@@ -0,0 +1,26 @@
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import Welcome from "../screens/Welcome/Index";
+import ShowCustomer from "../screens/ShowCustomer/Index";
+import RegionsList from "../screens/RegionsList/Index";
+import NewCustomer from "../screens/NewCustomer/Index";
+import EditCustomer from "../screens/EditCustomer/Index";
+import CustomersByRegion from "../screens/CustomersByRegion/Index";
+
+const Stack = createNativeStackNavigator();
+const Index = () => {
+  return (
+    <NavigationContainer initialRouteNam="Welcome">
+      <Stack.Navigator>
+        <Stack.Screen name="Welcome" component={Welcome} />
+        <Stack.Screen name="ShowCustomer" component={ShowCustomer} />
+        <Stack.Screen name="RegionsList" component={RegionsList} />
+        <Stack.Screen name="NewCustomer" component={NewCustomer} />
+        <Stack.Screen name="EditCustomer" component={EditCustomer} />
+        <Stack.Screen name="CustomersByRegion" component={CustomersByRegion} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default Index;
